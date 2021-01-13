83db6d5c53269d0a37a04811f047da99f3b27976
bharat2803 <67018724+bharat2803@users.noreply.github.com>
2021-01-13 14:25:28 +0530

    Add files via upload

---
+++ pdfwithpython/encryption.py
@@ -0,0 +1,25 @@
+import PyPDF2
+import getpass
+import os
+#import pdfwithpython.PDFfilepaths as pdf
+
+def encryptPDF(filepath,password):
+    head,tail=os.path.split(filepath)
+    with open(filepath, mode='rb') as file:
+        file_to_encrypt=PyPDF2.PdfFileReader(file)
+        if file_to_encrypt.isEncrypted:
+            return 1
+        else:
+            writer=PyPDF2.PdfFileWriter()
+            writer.cloneReaderDocumentRoot(file_to_encrypt)
+            writer.encrypt(password)
+            with open(head+"\\clone.pdf", mode='wb') as file:
+                writer.write(file)
+    os.rename(head+"\\clone.pdf",head+"\\"+tail.rsplit('.',1)[0]+"_encrypted.pdf")
+    print(head+"\\"+tail.rsplit('.',1)[0]+"_encrypted.pdf")
+    os.remove(filepath)
+    return 0
+    #os.startfile(filepath)
+
+if __name__ == "__main__":
+    encryptPDF(pdf.fetchpath("Resume.pdf"))
